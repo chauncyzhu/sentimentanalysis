4828e9c8bef7f69d700d4089201b2ad6b1a5eaef
chauncyzhu <chauncyzhu@163.com>
2017-04-16 22:17:21 +0800

    修改了文件目录路径
    对情感字典数据、语料库路径进行了修改

--- utils/sentiment_data_path.py
+++ utils/sentiment_data_path.py
@@ -4,8 +4,8 @@
 """
 SOURCE_PATH = "D:/Coding/pycharm-professional/pycharm-file/pythondata/sentiment/"
 #微博数据
-POS_WEIBO = SOURCE_PATH+"Weibo_four_sentimenteibo_four_sentiment/pos.csv"
-NEG_WEIBO = SOURCE_PATH+"Weibo_four_sentimenteibo_four_sentiment/neg.csv"
+POS_WEIBO = SOURCE_PATH+"weibo_four_sentiment/pos.csv"
+NEG_WEIBO = SOURCE_PATH+"weibo_four_sentiment/neg.csv"
 
 #产品评论数据
 NEG_COMMENT = SOURCE_PATH+"comment/neg.csv"
--- utils/sentiment_dict_path.py
+++ utils/sentiment_dict_path.py
@@ -2,3 +2,16 @@
 """
     情感分析字典的路径
 """
+SOURCE_PATH = "D:/Coding/pycharm-professional/pycharm-file/pythondata/sentiment/"
+
+#负面字典
+NEG_DICT = SOURCE_PATH + "sentimentDict/neg.csv"
+
+#正面字典
+POS_DICT = SOURCE_PATH + "sentimentDict/pos.csv"
+
+#程度字典
+PLUS_DICT = SOURCE_PATH + "sentimentDict/plus.csv"
+
+#否定字典
+NO_DICT = SOURCE_PATH + "sentimentDict/no.csv"
